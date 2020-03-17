6141da537dd3fbc8d4fe81ce7e296c7fa46b261c
andreea_crina05 <andreea_crina05@yahoo.com>
2020-03-17 15:46:30 +0200

    First commit

---
+++ src/main/java/com/example/demospringbootcrud/DemoSpringBootCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringbootcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demospringbootcrud/DemoSpringBootCrudApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.demospringbootcrud;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoSpringBootCrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
